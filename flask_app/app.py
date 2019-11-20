34387812da447099f90dd4f05c80ad142219f6a3
MikeTest <32805735+MikeTest@users.noreply.github.com>
2019-11-19 17:03:34 -0700

    Homework update

---
+++ flask_app/app.py
@@ -0,0 +1,43 @@
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#        MongoDB and Flask Application
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Import Dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#                Flask Setup
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+app = Flask(__name__)
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#          PyMongo Connection Setup
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#               Flask Routes
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Root Route to query MongoDB, pass mars data into HTML template index.html to display data
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# Scrape Route to import `scrape_mars.py` script & call `scrape` function
+@app.route("/scrape")
+def scrapper():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
+
+# Define main behavior
+if __name__ == "__main__":
+    app.run()
---
+++ flask_app/scrape_mars.py
@@ -0,0 +1,221 @@
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+# Jupyter Notebook conversion to python script
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Import Dependencies
+from bs4 import BeautifulSoup
+from splinter import Browser
+import pandas as pd
+import datetime as dt
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#                    Mac
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Set executable path & initialize chrome browser
+# executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+# browser = Browser("chrome", **executable_path, headless=False)
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#                  Windows
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Set executable path & initialize chrome browser
+executable_path = {"executable_path": "./chromedriver.exe"}
+browser = Browser("chrome", **executable_path)
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#               NASA Mars News
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# NASA mars news site web scraper
+def mars_news(browser):
+
+    # Visit the NASA Mars News Site
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    # Get first list item, wait half a second if not immediately present
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+    
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+
+    # Parse results HTML with BeautifulSoup
+    # Find Everything Inside:
+    #   <ul class="item_list">
+    #     <li class="slide">
+    try:
+        slide_element = news_soup.select_one("ul.item_list li.slide")
+        slide_element.find("div", class_="content_title")
+
+        # Scrape the latest news title
+        # Use parent element to find first <a> tag and save it as news_title
+        news_title = slide_element.find("div", class_="content_title").get_text()
+
+        news_paragraph = slide_element.find("div", class_="article_teaser_body").get_text()
+    except AttributeError:
+        return None, None
+    return news_title, news_paragraph
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#    JPL Mars Space Images - Featured Image
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# NASA JPL (Jet Propulsion Laboratory) site web scraper
+def featured_image(browser):
+
+    # Visit the NASA JPL (Jet Propulsion Laboratory) site
+    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(url)
+
+    # Ask Splinter to go to site and click button with class name full_image
+    # <button class="full_image">Full Image</button>
+    full_image_button = browser.find_by_id("full_image")
+    full_image_button.click()
+
+    # Find "More Info" button and Click It
+    browser.is_element_present_by_text("more info", wait_time=1)
+    more_info_element = browser.find_link_by_partial_text("more info")
+    more_info_element.click()
+
+    # Parse results HTML with BeautifulSoup
+    html = browser.html
+    image_soup = BeautifulSoup(html, "html.parser")
+
+    img = image_soup.select_one("figure.lede a img")
+    try:
+        img_url = img.get("src")
+    except AttributeError:
+        return None 
+   # Use base URL to create absolute URL
+    img_url = f"https://www.jpl.nasa.gov{img_url}"
+    return img_url
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#               Mars Weather
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Mars Weather Twitter account web scraper
+def twitter_weather(browser):
+
+    # Visit the Mars Weather Twitter account
+    url = "https://twitter.com/marswxreport?lang=en"
+    browser.visit(url)
+    
+    # Parse results HTML with BeautifulSoup
+    html = browser.html
+    weather_soup = BeautifulSoup(html, "html.parser")
+    
+    # Find a Tweet with the data-name `Mars Weather`
+    mars_weather_tweet = weather_soup.find("div", 
+                                       attrs={
+                                           "class": "tweet", 
+                                            "data-name": "Mars Weather"
+                                        })
+   # Search Within Tweet for <p> Tag Containing Tweet Text
+    mars_weather = mars_weather_tweet.find("p", "tweet-text").get_text()
+    return mars_weather
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#                 Mars Facts
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Mars Facts Web Scraper
+def mars_facts():
+
+    # Visit the Mars Facts site using Pandas to read
+    try:
+        df = pd.read_html("https://space-facts.com/mars/")[0]
+    except BaseException:
+        return None
+    df.columns=["Description", "Value"]
+    df.set_index("Description", inplace=True)
+
+    return df.to_html(classes="table table-striped")
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#              Mars Hemispheres
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+# Mars Hemispheres web scraper
+def hemisphere(browser):
+
+    # Visit the USGS Astrogeology Science center site
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    # Get a list of all the hemisphere
+    links = browser.find_by_css("a.product-item h3")
+    for item in range(len(links)):
+        hemisphere = {}
+        
+        # Find element on each loop to avoid stale element exception
+        browser.find_by_css("a.product-item h3")[item].click()
+        
+        # Find sample image anchor tag & extract href
+        sample_element = browser.find_link_by_text("Sample").first
+        hemisphere["img_url"] = sample_element["href"]
+        
+        # Get hemisphere title
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemisphere)
+        
+        # Navigate back
+        browser.back()
+    return hemisphere_image_urls
+
+# Helper Function
+def scrape_hemisphere(html_text):
+
+    hemisphere_soup = BeautifulSoup(html_text, "html.parser")
+    try: 
+        title_element = hemisphere_soup.find("h2", class_="title").get_text()
+        sample_element = hemisphere_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        title_element = None
+        sample_element = None 
+    hemisphere = {
+        "title": title_element,
+        "img_url": sample_element
+    }
+    return hemisphere
+
+
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+#            Main Web Scraping Bot
+# ◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘◘
+
+def scrape_all():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    browser = Browser("chrome", **executable_path, headless=False)
+    news_title, news_paragraph = mars_news(browser)
+    img_url = featured_image(browser)
+    mars_weather = twitter_weather(browser)
+    facts = mars_facts()
+    hemisphere_image_urls = hemisphere(browser)
+    timestamp = dt.datetime.now()
+
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": img_url,
+        "weather": mars_weather,
+        "facts": facts,
+        "hemispheres": hemisphere_image_urls,
+        "last_modified": timestamp
+    }
+    browser.quit()
+    return data 
+
+if __name__ == "__main__":
+    print(scrape_all())
